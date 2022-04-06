dd41f911a8f28573b822d91c39155881b62b040b
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.org>
2022-04-06 16:43:14 +0100

    Add project files and code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './app/App.js';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Import the store here.
+import { store } from './app/store.js';
+// Pass state and dispatch props to the <App /> component.
+const render = () => {
+  ReactDOM.render(
+    <App 
+      state={store.getState()}
+      dispatch={store.dispatch}
+    />,
+    document.getElementById('root')
+  )
+};
+render();
 
+// Subscribe render to the store.
+store.subscribe(render);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
